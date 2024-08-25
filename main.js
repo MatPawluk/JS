2ef49b33480c5040665ca9a701715e1b6a7b0063
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-25 18:10:08 +0200

    funkcje

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-const cars = "Audi, Mercedes, BMW, Nissan, Dogdge";
-
-const newCars = cars.split(", ");
-console.log(newCars);
-
-if (newCars.length > 3) {
-  console.log("Jest OK");
-} else {
-  console.log("Nie jest OK");
-}
-
-newCars.includes("Audi") ? newCars.push("Ford") : newCars.pop();
-console.log(newCars);
+const name = (params) => {};
