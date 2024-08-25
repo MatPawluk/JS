259f1259794e55f20673e8fee4864f050a7c2082
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-25 19:26:40 +0200

    task 1

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const name = (params) => {};
+let score;
+
+const add = (x, y) => {
+  score = x + y;
+  if (score % 2 === 0) {
+    console.log(`Liczba ${score} jest parzysta`);
+  } else {
+    console.log(`Liczba ${score} jest nieparzysta`);
+  }
+};
+
+add(5, 2);
