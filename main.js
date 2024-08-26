aad1587cfa693fc0837a121288984b40306fd081
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-26 11:31:15 +0200

    appendChild

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const heading = document.querySelector("h1");
-const par = document.querySelectorAll("p");
-const firstThreeParagraphs = Array.from(par).slice(0, 3);
-const div = document.querySelector(".test");
-const divP = div.querySelector("#test");
+const divTest = document.querySelector("div");
 
-console.log(heading);
-console.log(firstThreeParagraphs);
-console.log(div);
-console.log(divP);
+const par = document.createElement("p");
+
+divTest.appendChild(par);
