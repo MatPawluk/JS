87564606b286fb95e9884cead9baa498e7d6e8c0
Mateusz Pawluk <pawluk1988@icloud.com>
2024-08-26 15:54:33 +0200

    task 1

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-const btn = document.querySelector("button");
-const test = () => {
-  console.log("Hej");
-};
+const ulList = document.createElement("ul");
 
-btn.addEventListener("click", test);
+document.body.append(ulList);
+
+for (let i = 1; i <= 10; i++) {
+  const li = document.createElement("li");
+  const par = document.createElement("p");
+  par.textContent = i;
+  ulList.append(li);
+  li.append(par);
+}
+
+const lastLi = ulList.lastChild;
+const lastPar = lastLi.querySelector("p");
+
+lastLi.style.backgroundColor = "blue";
+lastLi.style.paddingBlock = "20px";
+lastLi.style.paddingInline = "40px";
+lastLi.style.fontSize = "48px";
+
+lastPar.textContent = "Jestem ostatnim paragrafem";
